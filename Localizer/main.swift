916d58d1f8d93a83d6f4860a55c57f7faccf0a9c
Ronny Gerasch <ronny.gerasch@gymondo.de>
2019-04-12 17:19:37 +0200

    file writing, sorting
    Signed-off-by: Ronny Gerasch <ronny.gerasch@gymondo.de>

--- Localizer/main.swift
+++ Localizer/main.swift
@@ -37,30 +37,61 @@ func parseFile(at path: URL) -> [String:String] {
 }
 
 // copy
+// TODO: - adjust regex, return outlet name as String
+func findIBOutlet(by id: String, in storyboard: URL) -> Bool {
+  do {
+    let content = try String(contentsOf: storyboard, encoding: .utf8)
+    let pattern = #"(<outlet).*(destination="\#(id)")"#
+    let regex = try NSRegularExpression(pattern: pattern, options: [])
+    let match = regex.firstMatch(in: content, range: NSRange(location: 0, length: content.utf16.count))
+    
+    return match != nil
+  } catch {
+    return false
+  }
+}
+
 func copyContent(_ content: [String:String], to replacement: [String:String]) -> [String:String] {
-  let sanitized = Dictionary(uniqueKeysWithValues: replacement.map { kv -> (key: String, value: String) in
+  let sanitized = replacement.map({ kv -> (key: String, value: String) in
     if let srcValue = content[kv.key] {
       return (kv.key, srcValue)
     }
     
+    let ibID = String(kv.key.split(separator: ".").first!)
+    
+    // TODO: get outlet name from following function
+//    let outlet = findIBOutlet(by: ibID, in: URL())
+    
+    // TODO: if outlet != nil, add name to comment
     return (kv.key, "\(kv.value) // TODO: check this")
   })
   
-  return sanitized
+  return Dictionary(uniqueKeysWithValues: sanitized)
 }
 
 // write .strings file
 func writeContent(_ content: [String:String], to path: URL) {
+  let fileContent = content.map { (key, value) -> String in
+    return "\(key) = \(value)"
+  }.sorted { lhs, rhs -> Bool in
+    let orderLhs = lhs.contains("// TODO: check this") ? 1 : 0
+    let orderRhs = rhs.contains("// TODO: check this") ? 1 : 0
+    
+    return orderLhs < orderRhs
+  }.joined(separator: "\n")
   
+  do {
+    try fileContent.write(to: path, atomically: true, encoding: .utf8)
+  } catch {
+    dump(error)
+    
+    exit(1)
+  }
 }
 
 let destinationFilePath = URL(fileURLWithPath: args[1])
 let destDict = parseFile(at: destinationFilePath) // japanese
 let srcDict = parseFile(at: URL(fileURLWithPath: args[2])) // german
-
 let sanitized = copyContent(srcDict, to: destDict)
 
-//let possibleUnused = srcDict.filter { !destDict.keys.contains($0.key) }
-
-// german file has 27 additional KV pairs
-// japanese file has 21 elements without pre-exising KV pairs
+writeContent(sanitized, to: destinationFilePath)
