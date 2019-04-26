60e68f3732185fcb5cbf23de5aef9e2b326eabf6
Alex Aldulea <aldulea.alexandru@gmail.com>
2019-04-26 11:46:59 +0200

    Attributes sanitization
    Added a sanitize function for attributes, because somtimes they migh contain chars like < > & \ "
    
    Solves: Generating invalid xml when invalid chars where found inside attributes value

--- parseXml.js
+++ parseXml.js
@@ -4,21 +4,36 @@ const convert = require('xml-js')
 
 const args = process.argv.slice(2)
 
-const options = { compact: false, spaces: 4 }
+function sanitize(unsafe) {
+    return unsafe.replace(/[<>&'"]/g, function (c) {
+        switch (c) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case '\'': return '&apos;';
+            case '"': return '&quot;';
+        }
+    })
+}
+
+const options = { compact: false, spaces: 4, attributeValueFn: sanitize }
 
 const localizableKey = value => JSON.parse(convert.xml2json(`<userDefinedRuntimeAttribute type="string" keyPath="localizableKey" value="${value}"/>`, options))
 const userDefinedAttrs = value => JSON.parse(convert.xml2json(`<userDefinedRuntimeAttributes><userDefinedRuntimeAttribute type="string" keyPath="localizableKey" value="${value}"/></userDefinedRuntimeAttributes>`, options))
 
 const processElement = (node, value) => {
     if (!node.elements) {
+        console.log('Node has no children, adding userDefinedAtts')
         node.elements = []
         node.elements.push(userDefinedAttrs(value).elements[0])
     } else {
         const attrIndex = node.elements
             .findIndex(element => element.name === 'userDefinedRuntimeAttributes')
         if (attrIndex >= 0) {
+            console.log('Node has userdefinedAttr, adding localizableKey to its children')
             node.elements[attrIndex].elements.push(localizableKey(value).elements[0])
         } else {
+            console.log('Node has no userdefinedAttr, adding userdefinedAttr')
             node.elements.push(userDefinedAttrs(value).elements[0])
         }
     }
@@ -27,8 +42,8 @@ const processElement = (node, value) => {
 function findNode(id, value, currentNode) {
     let i
     let result
-
     if (currentNode.attributes && id === currentNode.attributes.id) {
+        console.log('Found node with id', id)
         processElement(currentNode, value)
 
         return currentNode
@@ -78,10 +93,11 @@ rl.on('line', (line) => {
 }).on('close', () => {
     finalTranslations = ids.map((id, index) => ({ [id]: translations[index] }))
     args.forEach((path) => {
-        console.log('paths', path)
+        console.log('Reading path path:', path)
         fs.readdirSync(path).forEach((file) => {
             if (file.indexOf('.xib') > 1 || file.indexOf('.storyboard') > 1) {
                 const f = fs.readFileSync(path + file)
+                console.log('Processing: ', path + file)
                 const objFromFile = JSON.parse(convert.xml2json(f, options))
                 finalTranslations.forEach((translation) => {
                     Object.keys(translation).forEach(key => key.split(',').forEach((id) => {
@@ -94,5 +110,6 @@ rl.on('line', (line) => {
             }
         })
     })
+    console.log('Done :)' )
     process.exit(0)
 })
